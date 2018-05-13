a0f6fe9de2f635f685e141009a397a90b5275560
Nikhil Warke <nikwarke@gmail.com>
2018-05-13 11:26:59 +0530

    Add prod environment vars

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,6 @@
 module.exports = {
-  NODE_ENV: '"production"'
-}
+  NODE_ENV: '"production"',
+  WEB_SOCKET: '"wss://m14.cloudmqtt.com:38087/mqtt"',
+  SERVER_TOPIC: '"server"',
+  IOT_TOPIC: '"arduino"'
+};

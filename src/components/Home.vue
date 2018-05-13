c82c36a0318d8a1d40f53bcd2642668065dd9190
Nikhil Warke <nikwarke@gmail.com>
2018-05-13 11:57:43 +0530

    Add mqtt to home.vue
    After sending message on button click update color and icon of button
    if operation was successful

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container fluid fill-height>
     <v-layout wrap align-center>
-      <v-flex xs12 sm4 md4 v-for="device in devices" :key="device._id" text-xs-center>
-          <v-btn large :color="colorStyle(device.isOn)">{{ device.name }}
+      <v-flex xs12 sm4 md4 v-for="device in devices" :key="device.id" text-xs-center>
+          <v-btn @click.native="toggleState(device)" large :color="colorStyle(device.isOn)">{{ device.name }}
               <v-icon right>fas {{ iconStyle(device.isOn) }}</v-icon>
           </v-btn>
       </v-flex>
@@ -14,30 +14,52 @@
 export default {
   methods: {
     colorStyle: isOn => (isOn ? 'success' : 'error'),
-    iconStyle: isOn => (isOn ? 'fa-toggle-on' : 'fa-toggle-off')
+    iconStyle: isOn => (isOn ? 'fa-toggle-on' : 'fa-toggle-off'),
+    toggleState(device) {
+      const payload = {
+        device: device.id,
+        action: device.isOn ? 'off' : 'on',
+        sender: 'server'
+      };
+      this.$mqtt.publish(process.env.SERVER_TOPIC, JSON.stringify(payload));
+
+      this.devices.find(d => d.id === device.id).reqFlag = true;
+    }
+  },
+  mqtt: {
+    arduino(data) {
+      if (JSON.parse(data).message === 'done') {
+        const device = this.devices.find(d => d.reqFlag === true);
+        device.isOn = !device.isOn;
+        device.reqFlag = false;
+      } // else console.error('Request failed');
+    }
   },
   data: () => ({
     devices: [
       {
-        _id: 4,
+        id: 4,
         isOn: false,
         name: 'Fan',
-        req_flag: false
+        reqFlag: false
       },
       {
-        _id: 2,
+        id: 2,
         isOn: false,
         name: 'Room Lights',
-        req_flag: false
+        reqFlag: false
       },
       {
-        _id: 5,
+        id: 5,
         isOn: false,
         name: 'Outdoor Lights',
-        req_flag: false
+        reqFlag: false
       }
     ]
   }),
+  created() {
+    this.$mqtt.subscribe(process.env.IOT_TOPIC);
+  },
   props: {
     source: String
   }

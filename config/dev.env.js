da23fe5f4bfd107f62a46fc6d46074ad54ca0afe
Nikhil Warke <nikwarke@gmail.com>
2018-05-13 17:36:57 +0530

    Abadon trying to load creds secretly on frontend projectWebpack makes it hard to find credentials anyways

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,12 +1,8 @@
 'use strict';
 
 const merge = require('webpack-merge');
-const credentials = require('./cred.env');
 const prodEnv = require('./prod.env');
 
-module.exports = merge(
-  prodEnv,
-  Object.assign({}, credentials, {
-    NODE_ENV: '"development"'
-  })
-);
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"'
+});
--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,7 @@ module.exports = {
   NODE_ENV: '"production"',
   WEB_SOCKET: '"wss://m14.cloudmqtt.com:38087/mqtt"',
   SERVER_TOPIC: '"server"',
-  IOT_TOPIC: '"arduino"'
+  IOT_TOPIC: '"arduino"',
+  USERNAME: '"client"',
+  PASSWORD: '"client"'
 };

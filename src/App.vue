dce2db9837664b0a1e85e02039928c23230d44e8
Nikhil Warke <nwarke@protonmail.com>
2018-05-15 17:50:09 +0530

    Show progress circle when connection N/A
    When connection is not established, show a progress circle and a
    indicator in toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <v-app dark>
-    <v-toolbar app>
-      <v-spacer></v-spacer>
+    <v-toolbar app text-xs-center>
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-btn flat icon :color="isOnline">
+        <v-icon>fas fa-circle</v-icon>
+      </v-btn>
     </v-toolbar>
     <v-content>
-      <Home/>
+      <v-container v-if="isOnline === 'red'" fluid fill-height>
+        <v-layout wrap align-center>
+          <v-flex xs12 text-xs-center>
+            <v-progress-circular :size="70" :width="7" indeterminate color="red"/>
+          </v-flex>
+        </v-layout>
+      </v-container>
+      <Home v-else/>
     </v-content>
     <v-footer :fixed="fixed" app>
       <v-spacer></v-spacer>
@@ -17,14 +26,27 @@
 </template>
 
 <script>
+// import offline from 'v-offline';
 import Home from './components/Home';
 
 export default {
+  computed: {},
+  methods: {
+    connect(connack) {
+      this.isOnline = 'green';
+      /* eslint-disable no-console */
+      console.log(connack);
+    }
+  },
   data() {
     return {
       fixed: false,
-      title: 'AIDA Dashboard'
+      title: 'AIDA Dashboard',
+      isOnline: 'red'
     };
+  },
+  created() {
+    this.$mqtt.on('connect', this.connect);
   },
   name: 'App',
   components: {

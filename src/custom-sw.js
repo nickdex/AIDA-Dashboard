f9668c6a37bc0758e123d4aab268ba1fe3bdaed8
Nikhil Warke <nwarke@protonmail.com>
2018-07-04 10:21:56 +0530

    Add custom servcie
    Use push api to receive push notification from app server and
    notification api to show a notification with payload data
    
    GitHub #7

--- src/custom-sw.js
+++ src/custom-sw.js
@@ -1,6 +1,12 @@
 self.addEventListener('push', event => {
   console.info('Push received');
-  event.waitUntil(
-    self.registration.showNotification('Yo man', { data: 'Data' })
-  );
+
+  const payload = event.data.json();
+  console.debug(payload);
+  const options = {
+    body: payload.body,
+    icon: 'static/img/icons/android-chrome-192x192.png',
+    actions: [{ action: 'yes', title: 'Yes' }, { action: 'no', title: 'No' }]
+  };
+  event.waitUntil(self.registration.showNotification(payload.title, options));
 });

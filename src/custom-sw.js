735ba704427ed2b784f8e4dcd50d81359aa8d047
Nikhil Warke <nwarke@protonmail.com>
2018-07-05 10:18:52 +0530

    Handle notification click
    Server sends actions to be shown in notifications, send the selected
    action to server

--- src/custom-sw.js
+++ src/custom-sw.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 self.addEventListener('push', event => {
   console.info('Push received');
 
@@ -6,7 +7,36 @@ self.addEventListener('push', event => {
   const options = {
     body: payload.body,
     icon: 'static/img/icons/android-chrome-192x192.png',
-    actions: [{ action: 'yes', title: 'Yes' }, { action: 'no', title: 'No' }]
+    actions: payload.actions
   };
   event.waitUntil(self.registration.showNotification(payload.title, options));
 });
+
+self.addEventListener('notificationclick', event => {
+  console.debug('Inside Service worker');
+  const action = event.action;
+  console.info(`Action clicked: ${action}`);
+
+  if (action !== undefined) {
+    console.debug('Posting notification action using fetch api');
+
+    fetch('http://localhost:3000/push/click', {
+      method: 'POST',
+      body: self.JSON.stringify({ action }),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    })
+      .then(response => {
+        console.debug(`Response received: ${self.JSON.stringify(response)}`);
+
+        if (response.ok) {
+          console.info('Request to server successful');
+        } else console.warn('Request to server Failed');
+      })
+      .catch(err => console.error(err));
+  }
+});
+
+/* eslint-disable no-console */

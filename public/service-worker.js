111057ea47aa607a360b6f6001202818419d9ac7
Nikhil Warke <nwarke@protonmail.com>
2018-10-03 10:10:50 +0530

    Add pwa preset
    Use pwa plugin from vue cli along with eslint, unit testing, prettier, babel, vue router, vuex to generate boilerplate

---
+++ public/service-worker.js
@@ -0,0 +1,39 @@
+self.addEventListener('push', event => {
+  console.info('Push received');
+
+  const payload = event.data.json();
+  console.debug(payload);
+  const options = {
+    body: payload.body,
+    icon: 'static/img/icons/android-chrome-192x192.png',
+    actions: payload.actions
+  };
+  event.waitUntil(self.registration.showNotification(payload.title, options));
+});
+
+self.addEventListener('notificationclick', event => {
+  console.debug('Inside Service worker');
+  const action = event.action;
+  console.info(`Action clicked: ${action}`);
+
+  if (action !== undefined) {
+    console.debug('Posting notification action using fetch api');
+
+    fetch('https://aida-server.herokuapp.com/push/click', {
+      method: 'POST',
+      body: self.JSON.stringify({ action }),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    })
+      .then(response => {
+        console.debug(`Response received: ${self.JSON.stringify(response)}`);
+
+        if (response.ok) {
+          console.info('Request to server successful');
+        } else console.warn('Request to server Failed');
+      })
+      .catch(err => console.error(err));
+  }
+});
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  pwa: {
+    name: 'AIDA-Dashboard',
+    workboxPluginMode: 'InjectManifest'
+  }
+};

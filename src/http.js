c09424a90fb170ef58daf8b9b6a25fa1adf8f937
Nikhil Warke <nwarke@protonmail.com>
2018-10-03 10:45:28 +0530

    Add axios
    Add axios with proxy server set to heroku app when in prod and localhost otherwise

--- src/http.js
+++ src/http.js
@@ -4,9 +4,7 @@ export const kvHttp = axios.create({
   baseURL: 'https://api.keyvalue.xyz/'
 });
 
-export const httpClient = axios.create({
-  baseURL: process.env.SERVER_URL
-});
+export const httpClient = axios.create();
 
 export const all = requests => axios.all(requests);
 export const spread = callback => axios.spread(callback);
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,11 @@ module.exports = {
   pwa: {
     name: 'AIDA-Dashboard',
     workboxPluginMode: 'InjectManifest'
+  },
+  devServer: {
+    proxy:
+      process.env.NODE_ENV === 'production'
+        ? 'https://aida-server.herokuapp.com/'
+        : 'http://localhost:3000'
   }
 };

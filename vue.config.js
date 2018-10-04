6ebb726be541430e552066717b60714d5c301e48
Nikhil Warke <nwarke@protonmail.com>
2018-10-04 11:10:09 +0530

    Specify service woker file in vue config
    In Inject mode swSrc needs to have value of a service worker file

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
+const path = require('path');
+
 module.exports = {
   pwa: {
     name: 'AIDA-Dashboard',
-    workboxPluginMode: 'InjectManifest'
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: path.join('src', 'service-worker.js')
+    }
   },
   devServer: {
     proxy:

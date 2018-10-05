acdf0b1e7d8c2d27b2bddba3781a8027747bba3c
Nikhil Warke <nwarke@protonmail.com>
2018-10-05 10:57:19 +0530

    Use CDN fonts and icons
    Offline assets increases bundle size while CDN can be cached by service
    worker for offline use later

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import '@babel/polyfill';
-import 'roboto-fontface/css/roboto/roboto-fontface.css';
-import '@fortawesome/fontawesome-free/css/all.css';
 import Vue from 'vue';
 import './plugins/vuetify';
 import App from './App.vue';
